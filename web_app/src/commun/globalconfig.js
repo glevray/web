bab256f32baf89cde9f6f222c87156158c48fc69
Glevray <gerald.levray@free.fr>
2020-08-14 14:43:16 +0200

    apres ajout des derniers fichiers

---
+++ web_app/src/commun/globalconfig.js
@@ -0,0 +1,8 @@
+var globalConfig = {
+
+    URLACCES: 'http://127.0.0.1:5000/api/todos',
+    /*URLACCES: 'http://192.168.99.100:5000/api/todos',*/
+    /*URLINDEX: "../index.html",*/
+    URLINDEX: "../index.html",
+    version: 2
+};
